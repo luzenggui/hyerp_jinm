36ebacfd17dbacb42a571c68ac22051d6162552d
luzenggui <luren2007@163.com>
2020-11-15 17:22:01 +0800

    update

--- app/Http/Controllers/Development/GenbarcodeController.php
+++ app/Http/Controllers/Development/GenbarcodeController.php
@@ -117,12 +117,43 @@ class GenbarcodeController extends Controller
                 $objExcel = $reader->getExcel();
                 $sheet = $objExcel->getSheet(0);
                 $row=$startcell;
-                $rowData = $sheet->rangeToArray('B' . $row . ':' . 'B'.$endcell , NULL, TRUE, FALSE);
-
+                $rowData = $sheet->rangeToArray('A' . $row . ':' . 'A'.$endcell , NULL, TRUE, FALSE);
+                if(null !=$rowData)
+                    $rowData=$this->checksum($rowData);
 //            dd($rowData);
             });
         });
 //        dd($rowData);
         return view('development.genbarcode.labelprint', compact('rowData'));
+    }
+
+    static public function checksum($number)
+    {
+        foreach($number as $uccnumber)
+        {
+//            dd($uccnumber[0]);
+            $number1=substr($uccnumber[0],0,1);
+            $number3=substr($uccnumber[0],2,1);
+            $number5=substr($uccnumber[0],4,1);
+            $number7=substr($uccnumber[0],6,1);
+            $number9=substr($uccnumber[0],8,1);
+            $number11=substr($uccnumber[0],10,1);
+
+            $numberodd=($number1+$number3+$number5+$number7+$number9+$number11)*3;
+
+            $number2=substr($uccnumber[0],1,1);
+            $number4=substr($uccnumber[0],3,1);
+            $number6=substr($uccnumber[0],5,1);
+            $number8=substr($uccnumber[0],7,1);
+            $number10=substr($uccnumber[0],9,1);
+            $numberoven=$number2+$number4+$number6+$number8+$number10;
+
+            $numbermod=($numberodd+$numberoven)%10;
+            $checknum=10-$numbermod;
+//            dd($checknum);
+            $uccnumber[0] .=$checknum;
+        }
+
+        return $number;
     }
 }
